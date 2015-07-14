657e6959b0d7b17602b134055f10dbcbec101adc
Markus Schmidberger <schmidbe@amazon.de>
2015-07-14 05:01:34 +0000

    kinesis connection requires no more config

--- src/dashboard/kinesis_connect.py
+++ src/dashboard/kinesis_connect.py
@@ -1,8 +1,9 @@
 from boto import kinesis
+import urllib
 
-#REGION = 'us-west-2'
-REGION = 'eu-central-1'
-STREAMNAME = 'IoTSensorDemoEUCentral'
+# we assume that Kinesis is running in same region
+REGION = urllib.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone').read()[:-1]
+STREAMNAME = 'IoTSensorDemo'
 
 # Kinesis connection
 kinesisConn = kinesis.connect_to_region(REGION)
