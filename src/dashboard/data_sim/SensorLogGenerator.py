0fe48e65e10401c2d2a089e9f8150bb7d3618998
Markus Schmidberger <schmidbe@amazon.de>
2015-07-14 06:56:44 +0000

    adding amazon license info to all files

--- src/dashboard/data_sim/SensorLogGenerator.py
+++ src/dashboard/data_sim/SensorLogGenerator.py
@@ -1,8 +1,25 @@
 #! /usr/bin/python
 #
 # Markus Schmidberger, schmidbe@amazon.de
-# December 21, 2014
-# Simulate SensorLog (iphone app) Data and submit to Kinesis
+# July 14, 2015
+# Simulate SensorLog Data and submit to Kinesis
+#
+####################
+#
+# Copyright 2014, Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Amazon Software License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/asl/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+#####################
 
 import json
 from boto import kinesis
--- src/dashboard/data_sim/data_preprocessing.R
+++ src/dashboard/data_sim/data_preprocessing.R
@@ -1,5 +1,26 @@
-library(dplyr)
+#
+# Markus Schmidberger, schmidbe@amazon.de
+# July 14, 2015
+# Script to convert recorded data into correct format for simulation.
+#
+####################
+#
+# Copyright 2014, Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Amazon Software License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/asl/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+#####################
 
+library(dplyr)
 
 write.table(dataMotion, 
             file="data_sim/data/cleanSimData_Motion.csv", 
@@ -12,7 +33,5 @@ write.table(dataOrientation,
 write.table(dataGeo, 
             file="data_sim/data/cleanSimData_Geo.csv", 
             row.names = TRUE, sep=",")
-
-
 
 # manually add empty header at first position.
