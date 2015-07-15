b18088638d1757d4208ab66573488f8e68684d63
Markus Schmidberger <schmidbe@amazon.com>
2015-07-15 20:28:20 +0200

    moving to user data script

--- src/install/install_AmazonLinux.sh
+++ src/install/install_AmazonLinux.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 #
 # Markus Schmidberger, schmidbe@amazon.de
 # July 14, 2015
-# Install / bootstrap script for EC2 server. Installing
+# User Data / bootstrap script to install software on EC2 server. Installing
 # * R
 # * shiny
 # * required packages for shiny based IoT Sensor Demo dashboard
@@ -24,11 +25,10 @@
 #####################
 
 # update server
-sudo yum update -y
+yum update -y
 
 # install R
-sudo yum install R -y
-
+yum install R -y
 
 # install RStudio -> not required
 #wget http://download2.rstudio.org/rstudio-server-rhel-0.99.442-x86_64.rpm
@@ -37,46 +37,40 @@ sudo yum install R -y
 #sudo restart rstudio-server
 
 # install shiny package
-sudo su - \
--c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
+R -e "install.packages('shiny', repos='http://cran.rstudio.com/')"
 
 # install shiny server
 wget http://download3.rstudio.org/centos-5.9/x86_64/shiny-server-1.3.0.403-rh5-x86_64.rpm
-sudo yum install -y --nogpgcheck shiny-server-1.3.0.403-rh5-x86_64.rpm
+yum install -y --nogpgcheck shiny-server-1.3.0.403-rh5-x86_64.rpm
 
 # install packages required for IoT Demo
-sudo su - \
--c "R -e \"install.packages(c('DBI', 'BH'), repos='http://cran.rstudio.com/')\""
-
+R -e "install.packages(c('DBI', 'BH'), repos='http://cran.rstudio.com/')"
+R -e "install.packages(c('assertthat', 'magrittr', 'lazyeval'), repos='http://cran.rstudio.com/')"
 wget https://github.com/hadley/dplyr/archive/v0.4.0.tar.gz
-sudo R CMD INSTALL v0.4.0.tar.gz 
-
-sudo yum install -y libpng-devel
-sudo yum install -y libcurl-devel
-
-sudo su - \
--c "R -e \"install.packages(c('ggvis', 'rPython', 'shinydashboard', 'shinyBS', 'leaflet'), repos='http://cran.rstudio.com/')\""
-
-sudo su - \
--c "R -e \"install.packages(c('RCurl'), repos='http://cran.rstudio.com/')\""
+R CMD INSTALL v0.4.0.tar.gz
+yum install -y libpng-devel
+yum install -y libcurl-devel
+R -e "install.packages(c('ggvis', 'rPython', 'shinydashboard', 'shinyBS', 'leaflet','RCurl'), repos='http://cran.rstudio.com/')"
 
 
 # install git and clone repo
-sudo yum install -y git
-git clone https://github.com/schmidb/amazon-kinesis-IoT-sensor-demo.git
+yum install -y git
+mkdir /home/ec2-user/amazon-kinesis-IoT-sensor-demo
+git clone https://github.com/schmidb/amazon-kinesis-IoT-sensor-demo.git /home/ec2-user/amazon-kinesis-IoT-sensor-demo
+chown ec2-user:ec2-user -R /home/ec2-user/amazon-kinesis-IoT-sensor-demo
 
 # change config for www producer
-# sudo sed -i -- 's/arn:aws:iam::374311255271:role/Cognito_IoTSensorDemoUnauth_DefaultRole/YOURROLE/g' amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
-# sudo sed -i -- 's/374311255271/YOURACCOUNTID/g' amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
-# sudo sed -i -- 's/eu-west-1:b23b2b5b-b1c8-45ff-b7fb-3dd73c8f1466/YOURIDENTITYPOOL/g' amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
-# sudo sed -i -- 's/eu-west-1/YOURREGION/g' amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
+sed -i -- 's/arn:aws:iam::374311255271:role\/Cognito_IoTSensorDemoUnauth_DefaultRole/XXXXXXX/g' /home/ec2-user/amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
+sed -i -- 's/374311255271/XXXXXXX/g' /home/ec2-user/amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
+sed -i -- 's/eu-west-1:b23b2b5b-b1c8-45ff-b7fb-3dd73c8f1466/XXXXXX/g' /home/ec2-user/amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
+sed -i -- "s/'eu-west-1'/'XXXXXX'/g" /home/ec2-user/amazon-kinesis-IoT-sensor-demo/src/www/iotdemo.js
 
 # copy shiny-server config and restart shiny-server
-sudo cp amazon-kinesis-IoT-sensor-demo/src/install/shiny-server.conf /etc/shiny-server/shiny-server.conf
-sudo restart shiny-server
+cp /home/ec2-user/amazon-kinesis-IoT-sensor-demo/src/install/shiny-server.conf /etc/shiny-server/shiny-server.conf
+restart shiny-server
 
 # add time sync to avoid server running out of time
-sudo yum install -y ntp ntpdate ntp-doc
-sudo chkconfig ntpd on
-sudo ntpdate pool.ntp.org
-sudo /etc/init.d/ntpd start
+yum install -y ntp ntpdate ntp-doc
+chkconfig ntpd on
+ntpdate pool.ntp.org
+/etc/init.d/ntpd start
